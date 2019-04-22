9eac62626fab9ccffdb333b23ea0e0c78161bd97
Charles Korpics <korpx94@gmail.com>
2019-04-22 16:28:18 -0400

    did not save everything during last commit

--- theprojrouter/src/App.js
+++ theprojrouter/src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Link } from "react-router-dom";
+import Topics from './components/Topics'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      data: [{
+        agency: '',
+        date: '',
+        brief_description: ''
+      }],
+      topics: [],
+      other: [],
+      violence: [],
+      auto: [],
+      animals: [],
+      utility: [],
+      
+
+    }
+
+    this.handleClicker=this.handleClicker.bind(this);
+  }
+
+
+
+async handleClicker(e) {
+    try {
+    const resp = await fetch('https://data.cityofnewyork.us/resource/wewp-mm3p.json');
+    const data = await resp.json();
+    const data1 = data;
+    this.setState({
+      data: data1
+    });
+    return console.log(this.state.data,'that is the data');
+  }
+  catch (err) {
+    return console.log('hey there didnt work bud');
+  }
+  }
+
+componentWillMount(){
+  console.log('im runnin')
+
 }
 
+render() {
+  return(
+    <div className="App">
+        <nav>
+          <button onClick={this.handleClicker}>go get that data yo</button>
+          <p>
+            <Link to="/components/Topics">Topics</Link>
+          </p>
+        </nav>
+      <main>
+      <Route path="/components/Topics" component={
+    () => (<Topics data={this.state.data} num={this.state.num} />
+)}/>
+      </main>
+    </div>
+  )
+}
+}
 export default App;
