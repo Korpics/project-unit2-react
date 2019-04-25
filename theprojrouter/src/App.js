6a1dfc176866f79600b5c27fc2b4cb9d642c0462
Charles Korpics <korpx94@gmail.com>
2019-04-25 10:36:50 -0400

    mvp hopefully

--- theprojrouter/src/App.js
+++ theprojrouter/src/App.js
@@ -1,68 +1,124 @@
 import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
-import Topics from './components/Topics'
+import Random from "./Random"
+import SearchAll from "./SearchAll"
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      data: [{
-        agency: '',
-        date: '',
-        brief_description: ''
-      }],
-      topics: [],
-      other: [],
-      violence: [],
-      auto: [],
-      animals: [],
-      utility: [],
-      
-
+      data: [{}],
+      clicked: false,
+      next: 0,
+      src: "http://i.imgur.com/zivxHE1.gif"
     }
 
-    this.handleClicker=this.handleClicker.bind(this);
+    this.getFetch=this.getFetch.bind(this);
+    this.theSetter=this.theSetter.bind(this);
   }
 
-
-
-async handleClicker(e) {
+  async getFetch() {
     try {
     const resp = await fetch('https://data.cityofnewyork.us/resource/wewp-mm3p.json');
     const data = await resp.json();
     const data1 = data;
     this.setState({
       data: data1
-    });
-    return console.log(this.state.data,'that is the data');
-  }
+      });
+      return console.log(this.state.data,'api call');
+    }
   catch (err) {
-    return console.log('hey there didnt work bud');
+      return console.log('fetch broken.');
+    }  
   }
+  onHandleClick = async(event) => {
+    event.preventDefault();
+    const click = !this.state.clicked
+      await this.setState({ clicked: click })
+    console.log(click)
   }
 
-componentWillMount(){
-  console.log('im runnin')
+  onNextClick = (event) => {
+    event.preventDefault();
+    this.theSetter();
+    const nextint = this.state.next + 1
+    if(nextint > 3){
+      this.setState({ next: 0 })
+      } else {
+      this.setState({ next: nextint })
+    console.log(this.state.next)
+    }
+  }
 
+    theSetter(){
+      if(this.state.next === 1){
+      this.setState({ src: "https://i.imgur.com/fg91vTt.gif" })
+       console.log('thesetter', this.state.src)
+      }
+      else if (this.state.next === 2) {
+      this.setState({ src: "https://i.imgur.com/PowEW42.gif" })
+       console.log('thesetter', this.state.src)
+      }
+      else {
+      this.setState({ src: "http://i.imgur.com/zivxHE1.gif" })
+       console.log('thesetter', this.state.src)
+      }
+    }
+
+  componentDidMount(){
+  this.getFetch();
+  this.theSetter();
 }
 
 render() {
+  if(this.state.clicked === false){
   return(
-    <div className="App">
+    <div className="App" style={{display: 'flex', justifyContent: 'center', justifyContent: 'space-evenly', flexDirection: 'column'}}>
         <nav>
-          <button onClick={this.handleClicker}>go get that data yo</button>
-          <p>
-            <Link to="/components/Topics">Topics</Link>
-          </p>
+            <h3><Link to="/Random" style={{justifyContent: 'space-around', margin: 'auto'}}>RANDOM CALL!!</Link></h3>
+            <h3><Link to="/SearchAll"style={{justifyContent: 'space-around', margin: 'auto'}}>Search All</Link></h3>           
         </nav>
-      <main>
-      <Route path="/components/Topics" component={
-    () => (<Topics data={this.state.data} num={this.state.num} />
+      <article>
+    <Route exact path="/Random" component={
+    () => (<Random data={this.state.data} num={this.state.num} />
 )}/>
-      </main>
+    <Route exact path="/SearchAll" component={
+    () => (<SearchAll data={this.state.data} num={this.state.num} />
+)}/>
+
+      <button onClick={this.onHandleClick}>how I feel about the API I used</button>
+      </article>
+    </div>
+  )
+}
+else {
+  return(
+    <div className="App" style={{display: 'flex', justifyContent: 'center', justifyContent: 'space-evenly', flexDirection: 'column'}}>
+        <nav>
+            <h3><Link to="/Random" style={{justifyContent: 'space-around', margin: 'auto'}}>RANDOM CALL!!</Link></h3>
+            <h3><Link to="/SearchAll"style={{justifyContent: 'space-around', margin: 'auto'}}>Search All</Link></h3>           
+        </nav>
+      <article>
+    <Route exact path="/Random" component={
+    () => (<Random data={this.state.data} num={this.state.num} />
+)}/>
+    <Route exact path="/SearchAll" component={
+    () => (<SearchAll data={this.state.data} num={this.state.num} />
+)}/>
+    <container style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'column', justifyContent: 'space-between'}}>
+      <span><button onClick={this.onHandleClick}>clear image</button></span>
+     <div> <img src={this.state.src} alt="shucks" height="400" width="400"></img> </div>
+     <button onClick={this.onNextClick}>Next</button>
+    </container>     
+      </article>
     </div>
   )
 }
 }
+}
+
+
 export default App;
+
+  
---
+++ theprojrouter/src/SearchAll.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import { Route, Link } from "react-router-dom";
+import App from './App.js';
+
+class SearchAll extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            value: '',
+            filteredData: '',
+            toggler: false,
+            selection: 'default',
+            num: 1
+        }
+        this.handleChange=this.handleChange.bind(this)
+        this.searchFunction=this.searchFunction.bind(this)
+        this.onChange=this.onChange.bind(this)
+        this.handleNumClick=this.handleNumClick.bind(this)
+    }
+
+
+//switch case?
+   searchFunction = async(event) => {
+        event.preventDefault();
+        const searchables = ([...this.props.data])
+        console.log(this.state.selection)
+            if(this.state.selection === 'call_resolution'){
+        const results = searchables.filter(index => index.call_resolution.indexOf(this.state.value)>-1)
+        await this.setState({filteredData: results});
+        console.log('call res is set', this.state.filteredData)
+        }   else if (this.state.selection === 'agency'){
+        let results = searchables.filter(index => index.agency.toLowerCase().indexOf(this.state.value.toLowerCase())>-1)
+       await this.setState({filteredData: results});
+       console.log('agency is set', this.state.filteredData)
+        }   else if (this.state.selection === 'brief_description'){
+            console.log('brief desc is set', this.state.filteredData)
+        let results = searchables.filter(index => index.brief_description.toLowerCase().indexOf(this.state.value.toLowerCase())>-1)
+        await this.setState({filteredData: results});
+        console.log('brief desc is set', this.state.filteredData)
+        }   else {
+        let results = searchables.filter(index => index.brief_description.toLowerCase().indexOf(this.state.value.toLowerCase())>-1)
+        await this.setState({filteredData: results});          
+        }
+    }
+
+    onChange = async (event) => {
+        event.preventDefault();
+        console.log("Select list event: " + event.target.value);       
+        await this.setState({
+            selection: event.target.value,
+            toggler: true
+        });
+        console.log(this.state)
+    }
+
+    handleNumClick= async(event) => {
+        event.preventDefault();
+        const num = this.state.num
+        await this.setState({ num: Math.floor(Math.random() * this.state.filteredData.length) + 1  })
+        console.log(num)
+        }
+    
+    handleChange = async(event) => {
+        event.preventDefault();
+        await this.setState({value: event.target.value, filteredData: '', num: 1});
+        if(this.state.toggler === false){
+            await this.setState({toggler: true, filteredData: '', num: 1});
+        } else {
+            console.log(this.state.toggler)
+        }
+        console.log(this.state.value)
+      }
+
+    render() {
+        const filteredData = this.state.filteredData
+        const num = this.state.num
+        const toggler = this.state.toggler
+    if(filteredData.length >= 1 && filteredData[num] !== undefined)  {
+        return (
+            <div style={{ backgroundColor: "#90ee90"}}>
+            <nav><Link to="/App.js">Clear</Link></nav>
+            <Route exact path="/App.js" component={App} />
+                    <select defaultValue='brief_description' onChange={this.onChange}>
+                        <option value="brief_description">please select an option</option>
+                        <option value="brief_description">By Description</option>
+                        <option value="agency">By Agency</option>
+                        <option value="call_resolution">By Resolution</option>               
+                    </select>
+            <form value = {this.state.value} onSubmit={this.searchFunction} style={{backgroundColor: 'teal'}}>
+                    <label style={{color: 'teal'}}>
+                        Search All:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} style={{color: 'teal'}} />
+                    </label>
+                        <input type="submit" value="Submit" />
+            </form>
+            <div>
+                <button onClick={this.handleNumClick}>next call</button>
+                <span style={{fontColor: "#a0522d"}}>{num} of {filteredData.length}</span>
+                <div>
+                <h2> AGENCY: </h2>
+                <span> {filteredData[num].agency} </span>
+                </div>
+                <div>
+                <h2> Description of call: </h2>
+                <span> {filteredData[num].brief_description} </span>
+                </div>
+                <div>
+                <span><h2> Date of Call: </h2>
+                <span> {filteredData[num].date} </span>
+                <h2> Time: </h2>
+                <span> {filteredData[num].time} </span>
+                </span>
+                </div>
+                <div>
+                <h2> TLDR: </h2>
+                <span> {filteredData[num].inquiry_name} </span> 
+                </div>
+                <div>
+                <h2> Resolution: </h2>
+                <span> {filteredData[num].call_resolution} </span> 
+                </div>
+            </div>            
+        </div>
+        );
+    }
+    else if (filteredData !== '' && this.state.value.length >= 1 && filteredData.length <= 1){
+        return(
+            <div style={{ backgroundColor: "#cd5c5c"}}>
+            <nav><Link to="/App.js">Clear</Link></nav>
+            <Route exact path="/App.js" component={App} />
+                <select defaultValue='brief_description' onChange={this.onChange}>
+                    <option value="brief_description">please select an option</option>
+                    <option value="brief_description">By Description</option>
+                    <option value="agency">By Agency</option>
+                    <option value="call_resolution">By Resolution</option>               
+                </select>
+            <form value = {this.state.value} onSubmit={this.searchFunction}>
+                <label>
+                    Search All:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                    <input type="submit" value="Submit" />
+            </form>
+            <div>
+                <h3 style={{alignContent: 'center', fontSize: 90, fontFamily: "impact" }}>no data found please try again</h3>
+            </div>
+            </div>
+        )}
+        else if (num - 1 > filteredData.length && filteredData.length >= 1){
+            return(
+                <div style={{ backgroundColor: "#cd853f"}}>
+                <nav><Link to="/App.js">Clear</Link></nav>
+                <Route exact path="/App.js" component={App} />
+                    <select defaultValue='brief_description' onChange={this.onChange}>
+                        <option value="brief_description">please select an option</option>
+                        <option value="brief_description">By Description</option>
+                        <option value="agency">By Agency</option>
+                        <option value="call_resolution">By Resolution</option>               
+                    </select>
+                <form value = {this.state.value} onSubmit={this.searchFunction}>
+                    <label>
+                        Search All:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                        <input type="submit" value="Submit" />
+                </form>
+                <div>
+                    <h3 style={{alignContent: 'center', fontSize: 90, fontFamily: "impact" }}>I messed up</h3>
+                </div>
+                </div>
+            )}
+    else {
+        return(
+            <div style={{width: 600, minHeight: 300, backgroundColor: "#dcdcdc"}}>
+            <nav style={{marginTop: 80}}><Link to="/App.js">Clear</Link></nav>
+            <Route exact path="/App.js" component={App} />
+                <select defaultValue='brief_description' onChange={this.onChange} style={{marginTop: 30}}>
+                    <option value="brief_description">please select an option</option>
+                    <option value="brief_description">By Description</option>
+                    <option value="agency">By Agency</option>
+                    <option value="call_resolution">By Resolution</option>               
+                </select>
+                <form value = {this.state.value} onSubmit={this.searchFunction} style={{marginTop: 30}}>
+                    <label>
+                    Search All:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                <div>
+                    <h1 style={{fontFamily: 'comic sans', color: 'teal'}}> Please Select a Search Parameter</h1>
+                    <h3 style={{fontFamily: 'comic sans', color: 'teal'}}>then begin a search!</h3>
+                </div>
+            </div>   
+
+            )
+        }
+    }
+    }
+
+export default SearchAll;
